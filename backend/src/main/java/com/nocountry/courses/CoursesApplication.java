5f9e22812893f8c0d9657032f4b3c6fade1579c7
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2023-02-17 11:16:31 -0300

    Update CoursesApplication.java

--- backend/src/main/java/com/nocountry/courses/CoursesApplication.java
+++ backend/src/main/java/com/nocountry/courses/CoursesApplication.java
@@ -18,7 +18,7 @@ public class CoursesApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("");
+				registry.addMapping("/*").allowedOrigins("*");
 			}
 		};
 	}
