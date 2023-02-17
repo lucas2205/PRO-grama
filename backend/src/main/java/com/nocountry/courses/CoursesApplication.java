d95f8523a82ace433e8bbca5d106717753699a46
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2023-02-17 11:07:58 -0300

    Update CoursesApplication.java

--- backend/src/main/java/com/nocountry/courses/CoursesApplication.java
+++ backend/src/main/java/com/nocountry/courses/CoursesApplication.java
@@ -2,12 +2,25 @@ package com.nocountry.courses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CoursesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfiguration(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("");
+			}
+		};
 	}
 
 }
