bcabbf276aacd78800d2a0419a60e1999c65b70d
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2023-02-22 11:49:01 -0300

    Update CoursesApplication.java

--- backend/src/main/java/com/nocountry/courses/CoursesApplication.java
+++ backend/src/main/java/com/nocountry/courses/CoursesApplication.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class CoursesApplication {
 
