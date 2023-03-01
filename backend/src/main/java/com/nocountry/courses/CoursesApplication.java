9d4afaf18b73338b0b833586839fb477945ffbfb
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2023-03-01 12:20:28 -0300

    Update CoursesApplication.java

--- backend/src/main/java/com/nocountry/courses/CoursesApplication.java
+++ backend/src/main/java/com/nocountry/courses/CoursesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class CoursesApplication {
@@ -13,4 +14,8 @@ public class CoursesApplication {
 		SpringApplication.run(CoursesApplication.class, args);
 	}
 
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
+	}
 }
